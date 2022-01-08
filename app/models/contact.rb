5a7932a6bdd84bf1cd43996d56630724edf77f75
PedroHamzehT <pedrohamzeh@gmail.com>
2022-01-08 15:17:48 -0300

    Add uniqueness and format email validation and refactor the others validations

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,10 +4,13 @@ class Contact < ApplicationRecord
   encrypts :credit_card
 
   validates :name, :date_of_birth, :address, :credit_card, :franchise, :email, presence: true
-  validates :email, uniqueness: true
-  validate special_chars_on_name,
-           date_of_birth_format,
-           phone_format
+
+  validates :name,          format: { without: /[!"#$%&'()*+,-.\/:;<=>?@[\\]^_`{|}~]/, message: 'Invalid special character' }
+  validates :date_of_birth, format: { with: /^([0-9]{8}|[0-9]{4}-[0-9]{2}-[0-9]{2})$/, message: 'Invalid format' }
+  validates :phone,         format: { with: /^(\(\+[0-9]{2}\) [0-9]{3} [0-9]{3} [0-9]{2} [0-9]{2}|\(\+[0-9]{2}\) [0-9]{3}-[0-9]{3}-[0-9]{2}-[0-9]{2})$/, message: 'Invalid format' }
+  validates :email,         format: { with: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,3}$/, message: 'Invalid format' }
+
+  validate email_uniqueness
 
   before_save :find_franchise,
               :store_last_credit_card_numbers
@@ -22,21 +25,7 @@ class Contact < ApplicationRecord
     self.last_credit_card_numbers = credit_card.last(4)
   end
 
-  def special_chars_on_name
-    special_chars_regex = /[!"#$%&'()*+,-.\/:;<=>?@[\\]^_`{|}~]/
-
-    errors.add(:name, 'Invalid name - special character') if name[special_chars_regex]
-  end
-
-  def date_of_birth_format
-    date_format_regex = /^([0-9]{8}|[0-9]{4}-[0-9]{2}-[0-9]{2})$/
-
-    errors.add(:date_of_birth, 'Invalid date_of_birth format') unless date_of_birth[date_format_regex]
-  end
-
-  def phone_format
-    phone_format_regex = /^(\(\+[0-9]{2}\) [0-9]{3} [0-9]{3} [0-9]{2} [0-9]{2}|\(\+[0-9]{2}\) [0-9]{3}-[0-9]{3}-[0-9]{2}-[0-9]{2})$/
-
-    errors.add(:phone, 'Invalid phone format') unless phone[phone_format_regex]
+  def email_uniqueness
+    errors.add(:email, 'E-mail already exists') if find_by(email: email)
   end
 end
