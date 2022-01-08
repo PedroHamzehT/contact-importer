6d0a4e0990088d0450e03899bb734a41aa244131
PedroHamzehT <pedrohamzeh@gmail.com>
2022-01-08 14:26:58 -0300

    Add the global controller methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  def authenticate_user!
+    redirect_to '/users/new' unless session[:user_id]
+  end
+
+  def user_authenticated!
+    redirect_to '/contacts' if session[:user_id]
+  end
 end
