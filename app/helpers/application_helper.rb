76baaa5ac105281a86b57261037ed587cc8c4c6e
PedroHamzehT <pedrohamzeh@gmail.com>
2022-01-09 20:56:27 -0300

    Add some classes of bootstrap to all views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def authenticated_user?
     session[:user_id] ? true : false
   end
+
+  def current_controller?(controller)
+    request.path.include?(controller)
+  end
 end
