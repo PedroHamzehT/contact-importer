5e33abdba09092b38e37fb9b9edd62d4ce70e7ea
PedroHamzehT <pedrohamzeh@gmail.com>
2022-01-09 14:29:32 -0300

    Add current_user method to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
+  def current_user
+    @current_user ||= User.find_by_id(session[:user_id])
+  end
+
   def authenticate_user!
     redirect_to '/users/new' unless session[:user_id]
   end
