a4393548e8f0c45209c7fa5bb09f81dd281c1087
PedroHamzehT <pedrohamzeh@gmail.com>
2022-01-09 18:45:26 -0300

    Create test to verify enqueue job

--- spec/jobs/import_contacts_job_spec.rb
+++ spec/jobs/import_contacts_job_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe ImportContactsJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#perform_later' do
+    let(:import) { create(:import) }
+
+    let(:headers) do
+      { name: 'name', date_of_birth: 'date_of_birth', phone: 'phone', address: 'address', credit_card: 'credit_card', email: 'email' }
+    end
+
+    it 'must have enqueue job' do
+      ActiveJob::Base.queue_adapter = :test
+
+      expect {
+        described_class.perform_later(import.id, headers)
+      }.to have_enqueued_job.with(import.id, headers)
+    end
+  end
 end
