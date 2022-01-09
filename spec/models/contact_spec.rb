b55610e2b554a1162abae9947e792cff7ff8c9b0
PedroHamzehT <pedrohamzeh@gmail.com>
2022-01-09 00:57:40 -0300

    Add test to verify presence validation

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -12,4 +12,18 @@ RSpec.describe Contact, type: :model do
       expect { Contact.create(contact_attrs) }.to change { Contact.count }.by(1)
     end
   end
+
+  context 'missing values' do
+    let(:contact_fields) { %i[name date_of_birth phone address credit_card franchise email] }
+    let(:contact_attrs) { attributes_for(:contact) }
+
+    it 'must be invalid' do
+      contact_fields.each do |contact_field|
+        invalid_contact_attrs = contact_attrs
+        invalid_contact_attrs[contact_field] = nil
+
+        expect(Contact.new(invalid_contact_attrs).valid?).to be false
+      end
+    end
+  end
 end
