27f220ed821c6a9b5d659056443b39554da4f4bf
PedroHamzehT <pedrohamzeh@gmail.com>
2022-01-09 17:18:29 -0300

    Create list contacts page

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,5 +2,6 @@ class ContactsController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @contacts = Contact.where(user_id: current_user.id)
   end
 end
---
+++ app/helpers/contact_helper.rb
@@ -0,0 +1,9 @@
+module ContactHelper
+  def safe_fields
+    ['Name', 'Date of Birth', 'Phone', 'Address', 'Credit card last numbers', 'Franchise', 'E-mail']
+  end
+
+  def safe_attributes
+    %i[name date_of_birth phone address last_credit_card_numbers franchise email]
+  end
+end
