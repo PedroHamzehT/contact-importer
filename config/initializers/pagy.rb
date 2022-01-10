3d5c805fbfd74e088d980a98ccbfc84243edd17c
PedroHamzehT <pedrohamzeh@gmail.com>
2022-01-09 22:36:51 -0300

    Change default pagy items to 5 and fix email format validation

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Pagy::DEFAULT[:page]   = 1                                  # default
-Pagy::DEFAULT[:items]  = 20                                 # default
+Pagy::DEFAULT[:items]  = 5                                 # default
 Pagy::DEFAULT[:outset] = 0                                  # default
 
 Pagy::DEFAULT.freeze
